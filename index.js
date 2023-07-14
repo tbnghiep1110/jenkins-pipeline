6f2edf73c0ac9d1a3069213f8890a5585e9e76d0
tbnghiep1110 <95531035+tbnghiep1110@users.noreply.github.com>
2023-07-14 17:15:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Test Jenkins webhook');
+  res.end('Test Jenkins webhook qua met moi ');
 });
 
 server.listen(port, hostname, () => {
